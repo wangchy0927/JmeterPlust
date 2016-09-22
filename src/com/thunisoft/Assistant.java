b46a81642b6fa55bbb807e3969976c131324db74
Miracle <Miracle@172.16.176.36>
2016-09-22 10:32:09 +0800

    初始化

---
+++ src/com/thunisoft/Assistant.java
@@ -0,0 +1,45 @@
+package com.thunisoft;
+
+
+import org.apache.jmeter.assertions.AssertionResult;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+
+public class Assistant extends AbstractJavaSamplerClient {
+
+	private SampleResult results;
+
+	public Arguments getDefaultParameters() {
+		Arguments args = new Arguments();
+		args.addArgument("数学分数", "10");
+		args.addArgument("语文分数", "20");
+		args.addArgument("total", "23");
+		return args;
+	}
+
+	public void setupTest(JavaSamplerContext arg0) {
+	}
+	
+	public SampleResult runTest(JavaSamplerContext arg0) {
+		results = new SampleResult();
+		results.setSuccessful(true);
+		for(int i=0;i<=3;i++){
+			SampleResult subResult = new SampleResult();
+			int mathSocre = arg0.getIntParameter("数学分数");
+			int chinaSocre = arg0.getIntParameter("语文分数");
+			int total = arg0.getIntParameter("total");
+			int sum = mathSocre + chinaSocre + i;
+			boolean flag = sum == total;
+			if(flag == false)
+				results.setSuccessful(false);
+			subResult.setSuccessful(flag);
+			subResult.setSampleLabel("title:"+i);
+			subResult.setRequestHeaders("request");
+			subResult.setResponseData("response");
+			results.addSubResult(subResult);
+		}
+		return results;
+	}
+}
